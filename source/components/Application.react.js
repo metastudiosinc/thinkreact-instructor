4f7d4b8ed41307aeee84e963bafa001af4c6e400
Think React <thinkreact@Jamess-MacBook-Pro-2.local>
2017-01-07 18:11:58 -0500

    basic component

---
+++ source/components/Application.react.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Application = React.createClass({
+
+  render() {
+    return (
+      <div>React Application Component</div>
+    );
+  }
+
+
+
+});
+
+export default Application;
