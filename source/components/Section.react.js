0b1096ff3ed7072bab6c7807964bd32af4166534
Think React <thinkreact@Jamess-MacBook-Pro-2.local>
2017-01-20 21:04:30 -0500

    titles

--- source/components/Section.react.js
+++ source/components/Section.react.js
@@ -2,31 +2,32 @@ const React = require('react');
 var logs = require('../utilities/logMixin.js')
 const Section = React.createClass({
 
-  name: "Section",
-  mixins: [logs], //dont for get the comma
-  propTypes:{
-    title:React.PropTypes.string,
-    children:React.PropTypes.node
-  },
+    name: "Section", mixins: [logs], //dont for get the comma
+    propTypes: {
+        title: React.PropTypes.string,
+        children: React.PropTypes.node
+    },
 
-  //-----default API--------
-  //getDefaultProps: function(){},
-  //getInitialState: function() {},
-  //componentWillMount: function(){},
-  //componentDidMount: function(){},
-  //componentWillReceiveProps: function(){},
-  //shouldComponentUpdate: function(){},
-  //componentWillUpdate: function(){},
+    //-----default API--------
+    //getDefaultProps: function(){},
+    //getInitialState: function() {},
+    //componentWillMount: function(){},
+    //componentDidMount: function(){},
+    //componentWillReceiveProps: function(){},
+    //shouldComponentUpdate: function(){},
+    //componentWillUpdate: function(){},
 
-  render: function() {
-    return (
-      <div>SECTION {this.props.children}</div>
-    );
+    render: function() {
+        return (
+            <div>
+                <h2>{this.props.tite}</h2>
+                {this.props.children}
+            </div>
+        );
 
-  },
-  //componentDidUpdate: function(){},
-  //componentWillUnmount: function(){}
-
+    },
+    //componentDidUpdate: function(){},
+    //componentWillUnmount: function(){}
 
 });
 
